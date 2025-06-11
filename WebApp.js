424ace0f3e983b2437a415190e6f9c470c18a62a
vMigliorini <vicentemigliorini2007@gmail.com>
2025-06-11 20:08:37 -0300

    minha pica e groça

--- WebApp.js
+++ WebApp.js
@@ -1,68 +1,15 @@
 //alert("Oi") //Cria um alerta e interrompe toda a execução
 console.log("Script Inicializado");
 
-
 const botoesTipo = document.querySelectorAll('.tipoDoacao');
-const inputNome = document.getElementById("inputNome"); //Pega um elemento pelo ID
-const inputSenha = document.getElementById("inputSenha"); //Pega um elemento pelo ID; const = constante
-const btnSalvar = document.getElementById("btnSalvar"); //Pega um elemento pelo ID
-const divResponse = document.getElementById("response");
+const inputNome = document.getElementById("inptNome"); //Pega um elemento pelo ID
+const inputEmail = document.getElementById("inptEmail")
+const inputCelular = document.getElementById("inptCelular")
 const Head = document.getElementById("head")
 const btnPagamento = document.getElementById("btnPagamento")
 
-
-if (btnSalvar) {
-    btnSalvar.addEventListener('click', btnSalvarClick);
-}
-function btnSalvarClick(){
-    console.log("btn clicado")
-    readTxt() // cria uma execução asincrona, ele vai tudo que vem depois a mesmo tempo // tem o await para fazer as coisas esperarem a exxecução da função e torna a função principal em async
-    .then(reposta => { // arrow funcition
-        if (reposta){
-            console.log("Usuario Válido")
-            divResponse.innerHTML = ` 
-            <nav> 
-                <a href="./paginas/home.html" > Página Principal </a>
-            </nav>
-            `
-            Head.innerHTML += ` 
-                <meta http-equiv="refresh" content="0;URL='./paginas/home.html'">
-            `
-        }
-        else{ console.log("Usuario Invalido")
-            divResponse.innerHTML = `Usuário Inválido`
-        }
-    })
-
-}
-// ctrl + f, localizar
-async function readTxt(){ // async == asincrono, ele vai rodar no background, vai retornar uma promise
-    console.log("readTxt...")
-    
-    return fetch("credenciais.txt")    // promise
-    .then(resposta => resposta.text())                           // quando quiser um variavel declarar ela antes da flecha   // acessa o credenciais.txt
-    .then(text => {
-        return credValidate(text)                               // o then funciona para esperar o retorno do fetch, funciona como uma cascata
-    })
-    .catch(error => {
-        console.log("Erro: ",error)
-        return false
-    })
-}
-
-function credValidate(credenciais){
-    console.log("credValidate...")
-
-    let inptNome = inputNome.value; 
-    let inptSenha = inputSenha.value;
-
-    let txtNome = credenciais.split("/")[0] 
-    let txtSenha = credenciais.split("/")[1]
-
-    return inptNome == txtNome && inptSenha == txtSenha
-}
 botoesTipo.forEach(botao => {
-        botao.addEventListener('click', () => {
+        botao.addEventListener('click', () => {                 //código para fixar a cor nos botões de frequencia de doação
             // Remove a classe de todos os botões
             botoesTipo.forEach(b => b.classList.remove('botaoSelecionado'));
             // Adiciona a classe ao botão clicado
@@ -70,8 +17,28 @@ botoesTipo.forEach(botao => {
         });
     });
 
+    
+emailjs.init({
+  publicKey: "SUA_PUBLIC_KEY_AQUI",
+});
 btnPagamento.addEventListener('click', btnPagamentoClick);
 function btnPagamentoClick(){
     window.open('https://www.youtube.com/watch?v=LavcrI0F6LE', 'popupPagamento', 'width=600,height=400');
-
 }
+document.getElementById('btnAssinar').addEventListener('click', function(){
+    const email = inputEmail.value;
+    const nome = inputNome.value;
+    const templateParams = {
+        from_name: 'Vicente gostosão eco education', 
+        email_to: email, 
+        message: "oi, bem vindo a newsletter do eco education. Você vai receber Emails todo Domingo de manhã informando sobre atualizações do projeto",
+    };
+    emailjs.send('service_i3d8epn', 'template_4usbw5s', templateParams)
+        .then(function(response) {
+           console.log('SUCESSO!', response.status, response.text);
+           alert('Inscrição realizada com sucesso! Verifique seu e-mail.');
+        }, function(error) {
+           console.log('ERRO...', error);
+           alert('Ocorreu um erro ao realizar a inscrição. Tente novamente.');
+        })
+})
